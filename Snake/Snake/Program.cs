a30f3c347dfa1289c465f329cd2f7e7bcd351f09
Konstantin Savin <kostigan.savin@gmail.com>
2016-10-25 16:03:53 +0300

    Horisontal Line and Verical Line

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -16,6 +16,18 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
+            Point p3 = new Point(5, 7, '$');
+            p3.Draw();
+
+            Point p4 = new Point(7, 8, '@');
+            p4.Draw();
+
+            HorisontalLine hline = new HorisontalLine(5, 10, 8, '+');
+            hline.Drow();
+
+            VerticalLine vline = new VerticalLine(12, 5, 10, '=');
+            vline.Drow();
+    
             Console.ReadLine();
         }
     }
